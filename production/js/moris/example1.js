0a0d561847cd65912ee57e30d512f2738d92162b
Ezequias Rodrigues da Rocha <ezequias.rocha@gmail.com>
2015-05-29 17:34:00 -0300

    Translation update
    Translation update

--- production/js/moris/example1.js
+++ production/js/moris/example1.js
@@ -108,11 +108,11 @@ $(function () {
         hideHover: 'auto',
         lineColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
         data: [
-            {year: '2008', value: 0},
-            {year: '2009', value: 5},
-            {year: '2010', value: 10},
-            {year: '2011', value: 10},
-            {year: '2012', value: 16}
+            {year: '2008', value: 20},
+            {year: '2009', value: 10},
+            {year: '2010', value: 5},
+            {year: '2011', value: 5},
+            {year: '2012', value: 20}
         ]
     });
 
